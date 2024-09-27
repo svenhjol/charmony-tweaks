0e0ef399974e6b30498e247917edf53b836a63af
Sven H <sven.hjol@protonmail.com>
2024-09-27 21:11:25 +0000

    Initial commit

---
+++ src/main/java/svenhjol/charmony/tweaks/nano/fabric/ClientInitializer.java
@@ -0,0 +1,10 @@
+package svenhjol.charmony.tweaks.nano.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientInitializer implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
