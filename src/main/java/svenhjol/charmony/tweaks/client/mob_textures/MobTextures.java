84bf24982e373a3a69bea0cd82414264728e91f9
Sven H <sven.hjol@protonmail.com>
2024-10-03 17:16:32 +0100

    Change config switch labels for mob textures

--- src/main/java/svenhjol/charmony/tweaks/client/mob_textures/MobTextures.java
+++ src/main/java/svenhjol/charmony/tweaks/client/mob_textures/MobTextures.java
@@ -12,31 +12,31 @@ public final class MobTextures extends ModFeature {
     public final Handlers handlers;
     public final Registers registers;
 
-    @Configurable(name = "Cows", description = "If true, cows may spawn with alternative textures.")
+    @Configurable(name = "Cow textures", description = "If true, cows may spawn with alternative textures.")
     private static boolean cows = true;
 
-    @Configurable(name = "Chickens", description = "If true, chickens may spawn with alternative textures.")
+    @Configurable(name = "Chicken textures", description = "If true, chickens may spawn with alternative textures.")
     private static boolean chickens = true;
 
-    @Configurable(name = "Dolphins", description = "If true, dolphins may spawn with alternative textures.")
+    @Configurable(name = "Dolphin textures", description = "If true, dolphins may spawn with alternative textures.")
     private static boolean dolphins = true;
 
-    @Configurable(name = "Pigs", description = "If true, pigs may spawn with alternative textures.")
+    @Configurable(name = "Pig textures", description = "If true, pigs may spawn with alternative textures.")
     private static boolean pigs = true;
 
-    @Configurable(name = "Sheep", description = "If true, sheep face and 'shorn' textures match their wool color.")
+    @Configurable(name = "Sheep textures", description = "If true, sheep face and 'shorn' textures match their wool color.")
     private static boolean sheep = true;
 
-    @Configurable(name = "Snow golems", description = "If true, snow golems may spawn with alternative derp faces.")
+    @Configurable(name = "Snow golem face textures", description = "If true, snow golems may spawn with alternative derp faces.")
     private static boolean snowGolems = true;
 
-    @Configurable(name = "Squids", description = "If true, squids may spawn with alternative textures.")
+    @Configurable(name = "Squid textures", description = "If true, squids may spawn with alternative textures.")
     private static boolean squids = true;
 
-    @Configurable(name = "Turtles", description = "If true, turtles may spawn with alternative textures.")
+    @Configurable(name = "Turtle textures", description = "If true, turtles may spawn with alternative textures.")
     private static boolean turtles = true;
 
-    @Configurable(name = "Wandering traders", description = "If true, wandering traders may spawn with alternative textures.")
+    @Configurable(name = "Wandering trader textures", description = "If true, wandering traders may spawn with alternative textures.")
     private static boolean wanderingTraders = true;
 
     public boolean cows() {
