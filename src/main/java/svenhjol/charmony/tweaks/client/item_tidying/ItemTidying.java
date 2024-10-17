8445d8566e0fd1b13d8f5d10db515c5f25fa45df
Sven H <sven.hjol@protonmail.com>
2024-10-17 03:32:21 +0000

    Keybind to sort inventories

--- src/main/java/svenhjol/charmony/tweaks/client/item_tidying/ItemTidying.java
+++ src/main/java/svenhjol/charmony/tweaks/client/item_tidying/ItemTidying.java
@@ -1,20 +1,40 @@
 package svenhjol.charmony.tweaks.client.item_tidying;
 
+import svenhjol.charmony.core.annotations.Configurable;
 import svenhjol.charmony.core.annotations.FeatureDefinition;
 import svenhjol.charmony.core.base.Mod;
 import svenhjol.charmony.core.base.SidedFeature;
 import svenhjol.charmony.core.enums.Side;
+import svenhjol.charmony.tweaks.Tweaks;
 
-@FeatureDefinition(side = Side.Client, description = "Button to automatically stack and arrange items in inventory and containers.")
+@FeatureDefinition(side = Side.Client, description = """
+    Button to automatically stack and arrange items in inventory and containers.""")
 public final class ItemTidying extends SidedFeature {
     public final Registers registers;
     public final Handlers handlers;
     public final Providers providers;
+
+    @Configurable(
+        name = "Keybind sorts multiple inventories",
+        description = """
+            If true, the sorting key will sort player and non-player inventories in the same keypress.
+            If false, the sorting key only sorts non-player inventories unless viewing the player's inventory screen.""",
+        requireRestart = false
+    )
+    private static boolean keybindSortsMultipleInventories = false;
 
     public ItemTidying(Mod mod) {
         super(mod);
         handlers = new Handlers(this);
         registers = new Registers(this);
         providers = new Providers(this);
+    }
+
+    public static ItemTidying feature() {
+        return Tweaks.instance().feature(ItemTidying.class);
+    }
+
+    public boolean keybindSortsMultipleInventories() {
+        return keybindSortsMultipleInventories;
     }
 }
--- src/main/java/svenhjol/charmony/tweaks/client/item_tidying/Registers.java
+++ src/main/java/svenhjol/charmony/tweaks/client/item_tidying/Registers.java
@@ -1,21 +1,30 @@
 package svenhjol.charmony.tweaks.client.item_tidying;
 
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.components.WidgetSprites;
+import org.lwjgl.glfw.GLFW;
 import svenhjol.charmony.core.base.Setup;
 import svenhjol.charmony.core.events.RenderScreenCallback;
 import svenhjol.charmony.core.events.SetupScreenCallback;
 import svenhjol.charmony.tweaks.Tweaks;
 
 public class Registers extends Setup<ItemTidying> {
-    public final WidgetSprites tidyButton;
+    public final WidgetSprites tidyButtonSprite;
+    public final KeyMapping tidyInventoryKey;
 
     public Registers(ItemTidying feature) {
         super(feature);
 
-        tidyButton = new WidgetSprites(
+        this.tidyButtonSprite = new WidgetSprites(
             Tweaks.id("widget/item_tidying/tidy_button"),
             Tweaks.id("widget/item_tidying/tidy_button_highlighted")
         );
+
+        this.tidyInventoryKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+            "key.charmony-tweaks.tidyInventory",
+            GLFW.GLFW_KEY_APOSTROPHE,
+            "key.categories.inventory"));
     }
 
     @Override
